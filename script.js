9ad4a459af75e99df2c1aeb171cff3bd616657e6
JuliaKotlyar <julia.kotlyar2014@gmail.com>
2021-04-02 15:16:21 +0300

    added css

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ function computeLoan() {
     const month = document.getElementById('month').value;
     const interest = (amount * (interestRate * 0.01)) / month;
     let payment = (amount / month + interest).toFixed(2);
-    payment = payment.toString().replace(/\b(?=(\d{3})+(?!\d))/g, ','); //conver into money format
+    payment = payment.toString().replace(/\b(?=(\d{3})+(?!\d))/g, ''); //conver into money format
     document.getElementById('payment').innerHTML = 'Monthly Payment = $' + payment; // inject the result of the computed loan repayment
 }
